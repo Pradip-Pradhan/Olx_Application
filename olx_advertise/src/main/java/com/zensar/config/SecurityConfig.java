b345d4d3eb24556aa49eeb798cb2331e385508ad
pradip <PP66490@RMGW63ZLPC0495.ind.zensar.com>
2022-05-10 14:15:54 +0530

    httpSecurity

--- olx_advertise/src/main/java/com/zensar/config/SecurityConfig.java
+++ olx_advertise/src/main/java/com/zensar/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.zensar.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,6 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		return NoOpPasswordEncoder.getInstance();
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+	}
 	
 	
 }
